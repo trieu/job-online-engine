d12660c166002e773dcb1f46394aeb596aef506b
tantrieuf31@gmail.com <tantrieuf31@gmail.com@ab511a90-b563-11de-b1df-31c676d11d27>
2009-10-10 06:53:57 +0000

    Process Engine

---
+++ trunk/job-matcher/src/HttpClient.java
@@ -0,0 +1,251 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class HttpClient {
+	public void send(String s) {
+		try {
+			// Construct data
+			String data = URLEncoder.encode("key1", "UTF-8") + "="
+					+ URLEncoder.encode("value1", "UTF-8");
+			data += "&" + URLEncoder.encode("key2", "UTF-8") + "="
+					+ URLEncoder.encode("value2", "UTF-8");
+
+			// Send data
+			URL url = new URL("http://127.0.0.1/test/"+s);
+
+			URLConnection conn = url.openConnection();
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(conn
+					.getOutputStream());
+			wr.write(data);
+			wr.flush();
+
+			// Get the response
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn
+					.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				System.out.println(line);
+			}
+			wr.close();
+			rd.close();
+		} catch (Exception e) {
+		}
+
+	}
+
+	public static int invoke(String program) throws java.io.IOException,
+			java.lang.InterruptedException {
+		System.out.println("invoking program: " + program);
+		String s = null;
+		StringBuilder sb = new StringBuilder();
+		int exitValue = 0;
+		try {
+
+			// run the Unix "ps -ef" command
+			// using the Runtime exec method:
+			Process p = Runtime.getRuntime().exec(program);
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(
+					p.getErrorStream()));
+
+			// read the output from the command
+			System.out.println("Here is the standard output of the command:\n");
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+				sb.append(s);				
+			}
+			new HttpClient().send(sb.toString());
+
+			// read any errors from the attempted command
+			System.out.println("Here is the standard error of the command (if any):\n");
+			while ((s = stdError.readLine()) != null) {
+				System.out.println(s);
+			}
+			exitValue = p.exitValue();
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println("exception happened - here's what I know: ");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		return exitValue;
+	}
+
+	public static void testInvoke() {
+		try {
+			System.out.println("invoker start");
+			String cmd = "D:\\Program Files\\Notepad++Portable\\Notepad++Portable.exe";
+			cmd = "java -classpath C:\\ Hello Trieu";
+			int retval = invoke(cmd);
+			System.out.println("invoker end: returned " + retval);
+		} catch (java.io.IOException e) {
+			System.out.println("IOException caught: " + e);
+		} catch (java.lang.InterruptedException e) {
+			System.out.println("InterruptedException caught: " + e);
+		}
+	}
+
+	public static void main(String[] args) {
+		String testServerUrl = "http://127.0.0.1/test";
+		// HttpClient client = new HttpClient();
+		// client.send();
+
+		try {
+			// HTTPRequestPoster.sendDeleteRequest(testServerUrl, "");
+			// HTTPRequestPoster.sendGetRequest(testServerUrl, "");
+			// HTTPRequestPoster.postData(new StringReader("test r"), new
+			// URL(testServerUrl), new StringWriter());
+			testInvoke();
+		} catch (Exception e) {
+			e.printStackTrace();
+			// System.out.println(e.getMessage());
+		}
+
+	}
+
+}
+
+class HTTPRequestPoster {
+	/**
+	 * Sends an HTTP GET request to a url
+	 * 
+	 * @param endpoint
+	 *            - The URL of the server. (Example:
+	 *            " http://www.yahoo.com/search")
+	 * @param requestParameters
+	 *            - all the request parameters (Example:
+	 *            "param1=val1&param2=val2"). Note: This method will add the
+	 *            question mark (?) to the request - DO NOT add it yourself
+	 * @return - The response from the end point
+	 */
+	public static String sendGetRequest(String endpoint,
+			String requestParameters) {
+		String result = null;
+		if (endpoint.startsWith("http://")) {
+			// Send a GET request to the servlet
+			try {
+				// Construct data
+				StringBuffer data = new StringBuffer();
+				// Send data
+				String urlStr = endpoint;
+				if (requestParameters != null && requestParameters.length() > 0) {
+					urlStr += "?" + requestParameters;
+				}
+				URL url = new URL(urlStr);
+				URLConnection conn = url.openConnection();
+				// Get the response
+				BufferedReader rd = new BufferedReader(new InputStreamReader(
+						conn.getInputStream()));
+				StringBuffer sb = new StringBuffer();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
+				rd.close();
+				result = sb.toString();
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Reads data from the data reader and posts it to a server via POST
+	 * request. data - The data you want to send endpoint - The server's address
+	 * output - writes the server's response to output
+	 * 
+	 * @throws Exception
+	 */
+	public static void postData(Reader data, URL endpoint, Writer output)
+			throws Exception {
+		HttpURLConnection urlc = null;
+		try {
+			urlc = (HttpURLConnection) endpoint.openConnection();
+			try {
+				urlc.setRequestMethod("POST");
+			} catch (ProtocolException e) {
+				throw new Exception(
+						"Shouldn't happen: HttpURLConnection doesn't support POST??",
+						e);
+			}
+			urlc.setDoOutput(true);
+			urlc.setDoInput(true);
+			urlc.setUseCaches(false);
+			urlc.setAllowUserInteraction(false);
+			urlc.setRequestProperty("Content-type", "text/xml; charset="
+					+ "UTF-8");
+			OutputStream out = urlc.getOutputStream();
+			try {
+				Writer writer = new OutputStreamWriter(out, "UTF-8");
+				pipe(data, writer);
+				writer.close();
+			} catch (IOException e) {
+				throw new Exception("IOException while posting data", e);
+			} finally {
+				if (out != null)
+					out.close();
+			}
+			InputStream in = urlc.getInputStream();
+			try {
+				Reader reader = new InputStreamReader(in);
+				pipe(reader, output);
+				reader.close();
+			} catch (IOException e) {
+				throw new Exception("IOException while reading response", e);
+			} finally {
+				if (in != null)
+					in.close();
+			}
+		} catch (IOException e) {
+			throw new Exception("Connection error (is server running at "
+					+ endpoint + " ?): " + e);
+		} finally {
+			if (urlc != null)
+				urlc.disconnect();
+		}
+	}
+
+	public static void sendDeleteRequest(String endpoint,
+			String requestParameters) throws IOException {
+		URL url = new URL(endpoint);
+		HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestProperty("Content-Type",
+				"application/x-www-form-urlencoded");
+		httpCon.setRequestMethod("DELETE");
+		httpCon.connect();
+	}
+
+	/**
+	 * Pipes everything from the reader to the writer via a buffer
+	 */
+	private static void pipe(Reader reader, Writer writer) throws IOException {
+		char[] buf = new char[1024];
+		int read = 0;
+		while ((read = reader.read(buf)) >= 0) {
+			writer.write(buf, 0, read);
+		}
+		writer.flush();
+	}
+
+	String testServerUrl = "http://127.0.0.1/test";
+}

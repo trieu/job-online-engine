fb8049f3e4cd013631e9bdc3637532ecc2c1deb4
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2009-11-05 07:46:13 +0000

    improve page_decorator

--- job-online-website/application/views/admin/form_details.php
+++ job-online-website/application/views/admin/form_details.php
@@ -12,6 +12,7 @@
     }
     .input_info{
         border:medium solid!important;
+		padding: 11px;
     }
 </style>
 
@@ -41,7 +42,7 @@ echo renderInputField("FormID");
         <a href="javascript: showProcessList()">Set processes</a>
     </div>
     <?php
-    echo "<span style='margin:0px!important;'>".ul($selected_processes)."</span>";
+    echo ul($selected_processes);
     $selectBoxExtAttr = "id='ProcessID' size='4' multiple='multiple'";
     echo form_dropdown("ProcessID", $processes, $selected_processIDs, $selectBoxExtAttr);
     ?>
@@ -80,7 +81,55 @@ echo form_fieldset_close();
 ?>
 
 
-
+<style type="text/css">
+	#container_ui_box .selected_element {
+		background:#FFFF99 none repeat scroll 0 0;
+	}
+</style>
+<div id="container_ui_box">
+	<ul style="height:54px !important;" >
+		<li><a href="#tabs-1">Processes</a></li>
+		<li><a href="#tabs-2">Create new process</a></li>		
+	</ul>
+	<div id="tabs-1">
+		<div id="container_list" style="overflow-y:auto; height: 135px; margin-left: 15px; margin-bottom:5px;">
+			<?php foreach ($processes as $key  => $val) { ?>
+				<div style="margin-bottom:5px">
+					<input type="checkbox" id="element_<?= $key ?>" name="<?= $val ?>" onClick="container_ui_box.setSelectedRow(jQuery('#element_<?= $key ?>'))" />
+					<label for="element_<?= $key ?>" onClick="container_ui_box.setSelectedRow(jQuery('#element_<?= $key ?>'))" >	
+						<?= $val ?>
+					</label>
+				</div>
+			<?php }	?>	
+		</div>
+	</div>
+	<div id="tabs-2">
+		<div style="width: 90%; margin-left: 7px;">
+			<div>Name<span style="color: red;">*</span>:<br/>
+				<input id="containerName" type="text" style="width:100%"/>
+				<div id="nameErrorMsg" style="color:red; text-align:right" ></div>
+			</div>
+		</div>
+		<div style="text-align:center" >
+			<input type="button" value="OK" />
+			<input type="button" value="Cancel" />
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+	var container_ui_box = {};
+	container_ui_box.setSelectedRow = function(node){
+		if( jQuery(node).attr('checked') ){ 
+			jQuery(node).parent().addClass('selected_element');
+		}
+		else {
+			jQuery(node).parent().removeClass('selected_element');
+		}
+	};
+	jQuery(document).ready(function(){
+		jQuery("#container_ui_box").tabs();
+    });
+</script>
 
 <script type="text/javascript">
     var id = <?=  $id ?>;

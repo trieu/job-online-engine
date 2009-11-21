01c4df27f0d7e09bbd55cd76f9cf5f5d55e82eb9
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2009-11-21 04:19:31 +0000

    update codeigniter core

--- trunk/job-online-website/application/models/field_options_manager.php
+++ trunk/job-online-website/application/models/field_options_manager.php
@@ -1,41 +1,66 @@
 <?php
 
+require_once 'application/classes/FieldOption.php';
+
 /**
- * @property CI_Loader $load 
+ * @property CI_Loader $load
  * @property CI_DB_active_record $db
  */
 class field_options_manager extends data_manager {
 
     public function __construct() {
         parent::__construct();
-        $this->table_name = "fields";
+        $this->table_name = "fieldoptions";
     }
 
-   
-    public function find_by_filter($filter,$join_filter) {
+
+    public function find_by_filter($filter = array(), $join_filter = array()) {
+        return $this->select_db_helper($filter, $this->table_name, "FieldOption");
     }
+
 
     public function save($object) {
+
     }
-    
+
     public function find_by_id($id) {
+        $filter = array("FieldOptionID" => $id);
+        $list = $this->find_by_filter($filter);
+        if(sizeof($list) == 1) {
+            return $list[0];
+        }
+        return NULL;
     }
 
     public function get_dependency_instances() {
     }
-    
+
     public function delete($object) {
     }
 
-    protected function insert($object) {
+    protected function insert($data_array) {
+        $this->db->insert($this->table_name, $data_array);
+        if($this->db->affected_rows()>0) {
+            return $this->db->insert_id();
+        }
+        return -1;
     }
-    
+
     protected function update($object) {
+        $key_field_name = "FieldOptionID";
+        $id = $data_array[$key_field_name];
+        unset($data_array[$key_field_name]);
+        $this->db->where($key_field_name, $id);
+        $this->db->update($this->table_name, $data_array);
+        if($this->db->affected_rows()>0) {
+            return $id;
+        }
+        return -1;
     }
 
     public function delete_by_id($id) {
     }
-    
+
     public function updateByField($id,$editable_field_name,$editable_field_value) {
     }
 }

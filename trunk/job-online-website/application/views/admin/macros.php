f279f2d2c8144e7a2bb1222eb5fa0333db6c84f4
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2009-11-30 18:19:56 +0000

    improve admin management

--- trunk/job-online-website/application/views/admin/macros.php
+++ trunk/job-online-website/application/views/admin/macros.php
@@ -2,44 +2,53 @@
 <? function importContainerBox($container_name, $data_list, $is_multi_select) { ?>
 
 <style type="text/css">
-    #container_ui_box .selected_element {
+    .container_ui_box .selected_element {
         background:#FFFF99 none repeat scroll 0 0;
+    }
+    .jquery_tab_holder {
+        height:54px !important;
+    }
+    .jquery_tab_holder a{
+        font-size:15px!important;
     }
 </style>
 
-<div id="container_ui_box">
-    <ul style="height:54px !important;" >
-        <li><a href="#tabs-1"><?=$container_name?></a></li>
-        <li><a href="#tabs-2">Create new <?=$container_name?></a></li>
-    </ul>
-    <div id="tabs-1">
-        <div id="container_list" style="overflow-y:auto; height: 135px; margin-left: 15px; margin-bottom:5px;">
-            <?php foreach ($data_list as $key  => $val) { ?>
-            <div style="margin-bottom:5px">
-                <input type="checkbox" id="element_<?= $key ?>" name="<?= $val ?>" onClick="container_ui_box.setSelectedRow(jQuery('#element_<?= $key ?>'))" />
-                <label for="element_<?= $key ?>" onClick="container_ui_box.setSelectedRow(jQuery('#element_<?= $key ?>'))" >
-                        <?= $val ?>
-                </label>
-            </div>
-            <?php }	?>
-        </div>
-    </div>
-    <div id="tabs-2">
-        <div style="width: 90%; margin-left: 7px;">
-            <div>Name<span style="color: red;">*</span>:<br/>
-                <input id="containerName" type="text" style="width:100%"/>
-                <div id="nameErrorMsg" style="color:red; text-align:right" ></div>
+<textarea id="container_ui_box" cols="1" rows="1" style="display:none">
+    <div class="container_ui_box" >
+        <ul class="jquery_tab_holder"  >
+            <li><a href="#tabs-1"><?=$container_name?></a></li>
+            <li><a href="#tabs-2">Create new <?=$container_name?></a></li>
+        </ul>
+        <div id="tabs-1">
+            <div id="container_list" style="overflow-y:auto; height: 135px; margin-left: 15px; margin-bottom:5px;">
+                    <?php foreach ($data_list as $key  => $val) { ?>
+                <div style="margin-bottom:5px">
+                    <input type="checkbox" id="element_<?= $key ?>" name="<?= $val ?>" onClick="container_ui_box.setSelectedRow(Modalbox.contentSelector('#element_<?= $key ?>'))" />
+                    <label for="element_<?= $key ?>" onClick="container_ui_box.setSelectedRow(Modalbox.contentSelector('#element_<?= $key ?>'))" >
+                                <?= $val ?>
+                    </label>
+                </div>
+                    <?php }	?>
             </div>
         </div>
-        <div style="text-align:center" >
-            <input type="button" value="OK" />
-            <input type="button" value="Cancel" />
+        <div id="tabs-2">
+            <div style="width: 90%; margin-left: 7px;">
+                <div>Name<span style="color: red;">*</span>:<br/>
+                    <input id="containerName" type="text" style="width:100%"/>
+                    <div id="nameErrorMsg" style="color:red; text-align:right" ></div>
+                </div>
+            </div>
+            <div style="text-align:center" >
+                <input type="button" value="OK" />
+                <input type="button" value="Cancel" />
+            </div>
         </div>
     </div>
-</div>
+</textarea>
 
 <script type="text/javascript">
     var container_ui_box = {};
+
     container_ui_box.setSelectedRow = function(node){
         if( jQuery(node).attr('checked') ){
             jQuery(node).parent().addClass('selected_element');
@@ -48,9 +57,12 @@
             jQuery(node).parent().removeClass('selected_element');
         }
     };
-    jQuery(document).ready(function(){
-        jQuery("#container_ui_box").tabs();
-    });
+
+    container_ui_box.showBox = function(){        
+        Modalbox.show("#container_ui_box",{title:"<?=$container_name?>",height:430, width:600});
+        Modalbox.contentSelector(" > div").tabs();
+    }
+
 </script>
 
 <? } ?>
--- trunk/job-online-website/application/views/admin/objectclass_details.php
+++ trunk/job-online-website/application/views/admin/objectclass_details.php
@@ -21,10 +21,13 @@
 
 
 <?php
+require_once 'macros.php';
 addScriptFile("js/jquery.tokeninput/jquery.tokeninput.js");
+addScriptFile("js/commons.js");
 addCssFile("js/jquery.tokeninput/token-input.css");
 
 $obj = new ObjectClass();
+
 if(isset($obj_details)) {
     $obj = $obj_details;
 }
@@ -48,6 +51,23 @@ echo renderInputField("ObjectClassName","ObjectClassName",$obj->getObjectClassNa
 <div>
     <label for="Description">Description:</label>
     <textarea id ="Description" name="Description" rows="4" cols="50"><?= $obj->getDescription() ?></textarea>
+</div>
+
+<div id="data_suggestion_container">
+    <?php
+    $selected_processes = array();
+    $template = '<li class="token-input-token" ><p class="token-[id]">[name]</p><span class="token-input-delete-token">x</span></li>';
+    $tokens = "";
+    if(count($obj->getUsableProcesses())>0) {
+        foreach ($obj->getUsableProcesses() as $key => $val) {
+            $tem = str_replace( "[id]", $key,$template);
+            $tem = str_replace("[name]", $key." - ".$val->getProcessName(), $tem);
+            $tokens = $tokens . $tem;
+        }
+    }
+    ?>
+    <b>This object can do:</b>
+    <input type="text" id="data_suggestion" name="ProcessIDs" />
 </div>
 
 <div style="margin-top:32px">
@@ -75,8 +95,38 @@ echo form_fieldset_close();
         }
 
         jQuery("#ObjectClass_details").submit(function(){
-         
+            var ids = "";
+            jQuery("#data_suggestion_container p[class^='token-']").each(function(){
+                ids += (jQuery(this).attr("class").replace("token-","") + "&");
+            });
+            jQuery("#form_details input[name='ProcessIDs']").val(ids);
         });
+
+        jQuery("#data_suggestion").tokenInput("/job-online-website/index.php/admin/process_controller/getProcessesAsJson", {
+            hintText: "Type in the name of process",
+            noResultsText: "No results",
+            searchingText: "Searching...",
+            method: "POST"
+        });
+        jQuery("#data_suggestion_container").find(".token-input-list").prepend('<?= $tokens ?>');
+        jQuery("#data_suggestion_container .token-input-delete-token").click(function(){ jQuery(this).parent().remove();} );
+
+        var sortOpts = {
+            axis: "y",
+            containment: jQuery("#data_suggestion_container").find(".token-input-list"),
+            cursor: "move",
+            distance: 3
+        };
+        jQuery("#data_suggestion_container").find(".token-input-list").sortable(sortOpts);
     });
 
 </script>
+
+
+<?php
+$data_list= array();
+foreach ($available_processes as $p) {
+    $data_list[$p->getProcessID()] = $p->getProcessName();
+}
+echo importContainerBox("Available Process", $data_list, TRUE);
+ ?>

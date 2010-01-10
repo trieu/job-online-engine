4010c7e923ed6eec08567c80716556fde11fccac
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-01-10 06:24:51 +0000

    fix issue ID 2 "Can not save correctly data in "check box" Field type"

--- trunk/job-online-website/application/views/admin/object_do_form_view.php
+++ trunk/job-online-website/application/views/admin/object_do_form_view.php
@@ -62,26 +62,26 @@ addScriptFile("js/jquery/jquery.field.min.js");
                 if(jQuery(node_address).length > 1){
                     //hacking for checkbox
                     jQuery(node_address).each(function(){
-                        if(jQuery(this).attr("value") == object_field[id] )
+                        var isEquals = jQuery(this).attr("value") == object_field[id] ;
+                        //console.log(jQuery(this).attr("value") + " == " + object_field[id] +" isEquals " + isEquals);
+                        if(isEquals)
                         {
                            jQuery(this).attr("checked",true);
                            jQuery(this).attr("selected",true);
+                           var n = jQuery(this).attr("name") + "FVID_" + toks[1];
+                           jQuery(this).attr("name",n);
                         }
                         if(jQuery(this).attr("type") == "checkbox"){
-                            checkboxHashmap[jQuery(this).attr("id")] = true;
+                            checkboxHashmap[jQuery(this).attr("id")] = jQuery(this).attr("id");
                         }
                     });
                 }
                 else {
                     jQuery(node_address).setValue( object_field[id] );
+                    var n = jQuery(node_address).attr("name") + "FVID_" + toks[1];
+                    jQuery(node_address).attr("name",n);
                 }
-             };
-             for(var id in object_field){
-                var toks = id.split("FVID_");
-                var node_address = "#object_instance_form *[name='field_" + toks[0] +"']";
-                var n = jQuery(node_address).attr("name") + "FVID_" + toks[1];
-                jQuery(node_address).attr("name",n);
-             };
+             };            
              var f = function(){
                  var n = jQuery(this).attr("name");
                  if( n.split("FVID_").length < 2 ){
@@ -96,23 +96,31 @@ addScriptFile("js/jquery/jquery.field.min.js");
                  jQuery(this).attr("name",n);
              };
              jQuery("#object_instance_form *[name*='field_']").each(f);
-         }
-
+        }
         initSaveObjectForm();
      }
 
      function initSaveObjectForm(){
         var preSubmitCallback = function(formData, jqForm, options) {
             var data = {};
-            var records = [];
+            var records = {};
             for(var i in formData){
                 var toks = formData[i].name.split("FVID_");
                 var record = {};
                 record["FieldID"] =  new Number( toks[0].replace("field_",""));
                 record["FieldValueID"] =  new Number(toks[1]);
                 record["FieldValue"] = formData[i].value;
-                records.push(record);
+
+                var k = record["FieldID"] + "" +  record["FieldValueID"];
+                if(record["FieldValue"].length > 6){
+                    k +=  record["FieldValue"].substring(0,6);
+                }
+                else {
+                    k +=  record["FieldValue"];
+                }
+                records[k] = (record);
             }
+            console.log(formData);
             for(var id in checkboxHashmap){
                 var toks = jQuery("#" + id).attr("name").split("FVID_");
                 var record = {};
@@ -123,9 +131,16 @@ addScriptFile("js/jquery/jquery.field.min.js");
                 record["FieldID"] =  new Number( toks[0].replace("field_",""));
                 record["FieldValueID"] =  new Number(toks[1]);
                 record["FieldValue"] = value;
-                records.push(record);
+
+                var k = record["FieldID"]  + "" +  record["FieldValueID"] + record["FieldValue"];
+                records[k] = (record);
             }
-            data["FieldValues"] = jQuery.toJSON(records);
+            console.log(records);
+            var list = [];
+            for(var k in records){
+                list.push(records[k]);
+            }
+            data["FieldValues"] = jQuery.toJSON(list);
 
             var callback = function(responseText, statusText)  {
                 jQuery("#object_instance_div").append(responseText);
@@ -138,7 +153,47 @@ addScriptFile("js/jquery/jquery.field.min.js");
         };
 
         jQuery('#object_instance_form').submit(function() {
-            jQuery(this).ajaxSubmit({beforeSubmit: preSubmitCallback});
+            //jQuery(this).ajaxSubmit({beforeSubmit: preSubmitCallback});
+            var data = {};
+            data["FieldValues"] = [];
+            var tokens = jQuery(this).formSerialize().split("&");
+            var addedCheckBoxIds = {};
+            for(var i in tokens){               
+                var toks2 = tokens[i].split("=");
+                var toks3 = toks2[0].split("FVID_");
+                var record = {};
+                record["FieldID"] =  new Number( toks3[0].replace("field_",""));
+                record["FieldValueID"] =  new Number(toks3[1]);
+                record["FieldValue"] = toks2[1];
+                record["SelectedFieldValue"] = false;
+
+                var node = "input[type='checkbox'][name='" + toks2[0] + "']";
+                if( jQuery(node).length > 0 ){
+                    record["SelectedFieldValue"] = jQuery(node).attr("checked");
+                    addedCheckBoxIds[jQuery(node).attr("id")] = true;
+                }
+                data["FieldValues"].push(record);
+            }
+            for(var id in checkboxHashmap){
+                if(addedCheckBoxIds[id] != true) {
+                    var toks = jQuery("#" + id).attr("name").split("FVID_");
+                    var record = {};
+                    record["FieldID"] =  new Number( toks[0].replace("field_",""));
+                    record["FieldValueID"] =  new Number(toks[1]);
+                    record["FieldValue"] = jQuery("#" + id).val();
+                    record["SelectedFieldValue"] = jQuery("#" + id).attr("checked");
+                    data["FieldValues"].push(record);
+                }
+            }
+            data["FieldValues"] = jQuery.toJSON( data["FieldValues"] );
+            console.log(data);
+            var callback = function(responseText, statusText)  {
+                jQuery("#object_instance_div").append(responseText);
+                jQuery("#object_instance_div .ajax_loader").hide();
+            };
+            jQuery("#object_instance_div .ajax_loader").show();
+            jQuery("#object_instance_div form").hide();
+            jQuery.post( jQuery(this).attr("action"), data, callback );
             return false;
         });
     }

3a2e00ef0b41092020ef7b1aaa394ecce27a988b
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-01-13 16:52:55 +0000

    fix issue ID 2 "Can not save correctly data in "check box" Field type" -> Done

--- job-online-website/application/views/admin/create_object.php
+++ job-online-website/application/views/admin/create_object.php
@@ -90,26 +90,25 @@ foreach ($object_class->getUsableProcesses() as $pro) {
              var actionUrl = jQuery("#object_instance_form").attr("action") + "/" + ObjectID;
              jQuery("#object_instance_form").attr("action",actionUrl);
              for(var id in object_field) {
-                var toks = id.split("FVID_");
-                var node_address = "#object_instance_form *[name='field_" + toks[0] +"']";
+                var node_address = "#object_instance_form *[name='field_" + object_field[id].FieldID +"']";
 
-                if(jQuery(node_address).length > 1){
-                    //hacking for checkbox
-                    jQuery(node_address).each(function(){
-                        if(jQuery(this).attr("value") == object_field[id] )
-                        {
-                           jQuery(this).attr("checked",true);
-                           jQuery(this).attr("selected",true);
-                        }
-                        if(jQuery(this).attr("type") == "checkbox"){
-                            checkboxHashmap[jQuery(this).attr("id")] = true;
-                        }
-                    });
+                //hacking for checkbox
+                if( jQuery(node_address).attr("type") == "checkbox" ){
+                    node_address += "[value='" + object_field[id].FieldValue + "']"
+                    if(object_field[id].SelectedFieldValue == 1){
+                        jQuery(node_address).attr("checked",true);
+                        jQuery(node_address).attr("selected",true);
+                    }
+                    checkboxHashmap[jQuery(node_address).attr("id")] = jQuery(node_address).attr("id");
+                    var n = jQuery(node_address).attr("name") + "FVID_" + object_field[id].FieldValueID;
+                    jQuery(node_address).attr("name",n);
                 }
                 else {
-                    jQuery(node_address).setValue( object_field[id] );
+                    jQuery(node_address).setValue( object_field[id].FieldValue );
+                    var n = jQuery(node_address).attr("name") + "FVID_" + object_field[id].FieldValueID;
+                    jQuery(node_address).attr("name",n);
                 }
-             };
+             }; 
              for(var id in object_field){
                 var toks = id.split("FVID_");
                 var node_address = "#object_instance_form *[name='field_" + toks[0] +"']";
@@ -130,38 +129,47 @@ foreach ($object_class->getUsableProcesses() as $pro) {
                  jQuery(this).attr("name",n);
              };
              jQuery("#object_instance_form *[name*='field_']").each(f);
-         }
-
-         jQuery("#accordion").accordion({ collapsible: true });
+         }         
          initSaveObjectForm();
-
+         
+         jQuery("#accordion").accordion({ collapsible: true });
      }
 
      function initSaveObjectForm(){
-        var preSubmitCallback = function(formData, jqForm, options) {
+        jQuery('#object_instance_form').submit(function() {
+            //jQuery(this).ajaxSubmit({beforeSubmit: preSubmitCallback});
             var data = {};
-            var records = [];
-            for(var i in formData){
-                var toks = formData[i].name.split("FVID_");
+            data["FieldValues"] = [];            
+            var addedCheckBoxIds = {};
+
+            var hashmap = jQuery(this).serializeArray();
+            for(var i=0; i< hashmap.length; i++){
+                var toks3 = hashmap[i].name.split("FVID_");
                 var record = {};
-                record["FieldID"] =  new Number( toks[0].replace("field_",""));
-                record["FieldValueID"] =  new Number(toks[1]);
-                record["FieldValue"] = formData[i].value;
-                records.push(record);
+                record["FieldID"] =  new Number( toks3[0].replace("field_",""));
+                record["FieldValueID"] =  new Number(toks3[1]);
+                record["FieldValue"] = hashmap[i].value;
+                record["SelectedFieldValue"] = false;
+
+                var node = "input[type='checkbox'][name='" + hashmap[i].name + "']";
+                if( jQuery(node).length > 0 ){
+                    record["SelectedFieldValue"] = jQuery(node).attr("checked");
+                    addedCheckBoxIds[jQuery(node).attr("id")] = true;
+                }
+                data["FieldValues"].push(record);
             }
             for(var id in checkboxHashmap){
-                var toks = jQuery("#" + id).attr("name").split("FVID_");
-                var record = {};
-                var value = -1;
-                if( jQuery("#" + id).attr("checked")){
-                    value = jQuery("#" + id).val();
+                if(addedCheckBoxIds[id] != true) {
+                    var toks = jQuery("#" + id).attr("name").split("FVID_");
+                    var record = {};
+                    record["FieldID"] =  new Number( toks[0].replace("field_",""));
+                    record["FieldValueID"] =  new Number(toks[1]);
+                    record["FieldValue"] = jQuery("#" + id).val();
+                    record["SelectedFieldValue"] = jQuery("#" + id).attr("checked");
+                    data["FieldValues"].push(record);
                 }
-                record["FieldID"] =  new Number( toks[0].replace("field_",""));
-                record["FieldValueID"] =  new Number(toks[1]);
-                record["FieldValue"] = value;
-                records.push(record);
             }
-            data["FieldValues"] = jQuery.toJSON(records);
+            data["FieldValues"] = jQuery.toJSON( data["FieldValues"] );
 
             var callback = function(responseText, statusText)  {
                 jQuery("#object_instance_div").append(responseText);
@@ -169,12 +177,7 @@ foreach ($object_class->getUsableProcesses() as $pro) {
             };
             jQuery("#object_instance_div .ajax_loader").show();
             jQuery("#object_instance_div form").hide();
-            jQuery.post( jQuery(jqForm).attr("action"), data, callback );
-            return false;
-        };
-
-        jQuery('#object_instance_form').submit(function() {
-            jQuery(this).ajaxSubmit({beforeSubmit: preSubmitCallback});
+            jQuery.post( jQuery(this).attr("action"), data, callback );
             return false;
         });
     }

e7aec25e3b63d4d20ee8bd42f6b1ae656e960c1b
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-01-13 17:06:14 +0000

    add "Serialize Form to JSON" plugin

--- trunk/job-online-website/assets/js/commons.js
+++ trunk/job-online-website/assets/js/commons.js
@@ -140,3 +140,19 @@ var GUI = {};
 GUI.toggletVisible = function(selector){
     return jQuery(selector).toggleClass("not_visible");
 }
+
+jQuery.fn.serializeObject = function() {
+   var o = {};
+   var a = this.serializeArray();
+   jQuery.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           o[this.name].push(this.value || '');
+       } else {
+           o[this.name] = this.value || '';
+       }
+   });
+   return o;
+};

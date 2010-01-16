887b58b2b05b40cb8555f5ed1797bde3cb6c5a74
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-01-16 06:32:40 +0000

    fix issue ID 2 "Can not save correctly data in "check box" Field type" -> Done

--- job-online-website/application/views/admin/object_do_form_view.php
+++ job-online-website/application/views/admin/object_do_form_view.php
@@ -56,18 +56,20 @@ addScriptFile("js/jquery/jquery.field.min.js");
              jQuery("#object_instance_form").attr("action",actionUrl);
              
              for(var id in object_field) {                
-                var node_address = "#object_instance_form *[name='field_" + object_field[id].FieldID +"']";
+                var node_address = "#object_instance_form *[name*='field_" + object_field[id].FieldID +"']";
                 
                 //hacking for checkbox
                 if( jQuery(node_address).attr("type") == "checkbox" ){
-                    node_address += "[value='" + object_field[id].FieldValue + "']"
-                    if(object_field[id].SelectedFieldValue == 1){
-                        jQuery(node_address).attr("checked",true);
-                        jQuery(node_address).attr("selected",true);
+                    node_address += "[value='" + object_field[id].FieldValue + "']";
+                    if(jQuery(node_address).length >0 ){
+                        if(object_field[id].SelectedFieldValue == 1){
+                            jQuery(node_address).attr("checked",true);
+                            jQuery(node_address).attr("selected",true);
+                        }
+                        checkboxHashmap[jQuery(node_address).attr("id")] = jQuery(node_address).attr("id");
+                        var n = jQuery(node_address).attr("name") + "FVID_" + object_field[id].FieldValueID;
+                        jQuery(node_address).attr("name",n);
                     }
-                    checkboxHashmap[jQuery(node_address).attr("id")] = jQuery(node_address).attr("id");
-                    var n = jQuery(node_address).attr("name") + "FVID_" + object_field[id].FieldValueID;
-                    jQuery(node_address).attr("name",n);                    
                 }
                 else {
                     jQuery(node_address).setValue( object_field[id].FieldValue );
@@ -96,46 +98,52 @@ addScriptFile("js/jquery/jquery.field.min.js");
      function initSaveObjectForm(){
         jQuery('#object_instance_form').submit(function() {
             //jQuery(this).ajaxSubmit({beforeSubmit: preSubmitCallback});
-            var data = {};
-            data["FieldValues"] = [];
-            var addedCheckBoxIds = {};
+            try {
+                var data = {};
+                data["FieldValues"] = [];
+                var addedCheckBoxIds = {};
 
-            var hashmap = jQuery(this).serializeArray();
-            for(var i=0; i< hashmap.length; i++){
-                var toks3 = hashmap[i].name.split("FVID_");
-                var record = {};
-                record["FieldID"] =  new Number( toks3[0].replace("field_",""));
-                record["FieldValueID"] =  new Number(toks3[1]);
-                record["FieldValue"] = hashmap[i].value;
-                record["SelectedFieldValue"] = false;
-
-                var node = "input[type='checkbox'][name='" + hashmap[i].name + "']";
-                if( jQuery(node).length > 0 ){
-                    record["SelectedFieldValue"] = jQuery(node).attr("checked");
-                    addedCheckBoxIds[jQuery(node).attr("id")] = true;
-                }
-                data["FieldValues"].push(record);
-            }
-            for(var id in checkboxHashmap){
-                if(addedCheckBoxIds[id] != true) {
-                    var toks = jQuery("#" + id).attr("name").split("FVID_");
+                var hashmap = jQuery(this).serializeArray();
+                for(var i=0; i< hashmap.length; i++){
+                    var toks3 = hashmap[i].name.split("FVID_");
                     var record = {};
-                    record["FieldID"] =  new Number( toks[0].replace("field_",""));
-                    record["FieldValueID"] =  new Number(toks[1]);
-                    record["FieldValue"] = jQuery("#" + id).val();
-                    record["SelectedFieldValue"] = jQuery("#" + id).attr("checked");
+                    record["FieldID"] =  new Number( toks3[0].replace("field_",""));
+                    record["FieldValueID"] =  new Number(toks3[1]);
+                    record["FieldValue"] = hashmap[i].value;
+                    record["SelectedFieldValue"] = false;
+
+                    var node = "input[type='checkbox'][name='" + hashmap[i].name + "']";
+                    if( jQuery(node).length > 0 ){
+                        record["SelectedFieldValue"] = jQuery(node).attr("checked");
+                        addedCheckBoxIds[jQuery(node).attr("id")] = true;
+                    }
                     data["FieldValues"].push(record);
                 }
-            }
-            data["FieldValues"] = jQuery.toJSON( data["FieldValues"] );
+                for(var id in checkboxHashmap){
+                    if(id != null &&  addedCheckBoxIds[id] != true) {
+                        var toks = jQuery("#" + id).attr("name").split("FVID_");
+                        var record = {};
+                        record["FieldID"] =  new Number( toks[0].replace("field_",""));
+                        record["FieldValueID"] =  new Number(toks[1]);
+                        record["FieldValue"] = jQuery("#" + id).val();
+                        record["SelectedFieldValue"] = jQuery("#" + id).attr("checked");
+                        data["FieldValues"].push(record);
+                    }
+                }
+                data["FieldValues"] = jQuery.toJSON( data["FieldValues"] );
 
-            var callback = function(responseText, statusText)  {
-                jQuery("#object_instance_div").append(responseText);
-                jQuery("#object_instance_div .ajax_loader").hide();
-            };
-            jQuery("#object_instance_div .ajax_loader").show();
-            jQuery("#object_instance_div form").hide();
-            jQuery.post( jQuery(this).attr("action"), data, callback );
+                var callback = function(responseText, statusText)  {
+                    jQuery("#object_instance_div").append(responseText);
+                    jQuery("#object_instance_div .ajax_loader").hide();
+                };
+                jQuery("#object_instance_div .ajax_loader").show();
+                jQuery("#object_instance_div form").hide();
+                jQuery.post( jQuery(this).attr("action"), data, callback );
+            } catch (exception) {
+//                if(console){
+//                    console.log(exception);
+//                }
+            }
             return false;
         });
     }

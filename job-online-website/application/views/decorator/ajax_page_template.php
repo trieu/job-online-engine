1aa917103aff42b8548fb972cde58c04753a8fdf
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-01-24 04:58:00 +0000

    update search

--- job-online-website/application/views/decorator/ajax_page_template.php
+++ job-online-website/application/views/decorator/ajax_page_template.php
@@ -6,24 +6,25 @@
         <meta name="<?php echo $name;?>" content="<?php echo $content;?>" />
         <?php } ?>
         <base href="<?= base_url()?>" />
+        <title></title>
 
-        <link type="text/css" media="screen" rel="stylesheet" href="<?= base_url() ?>assets/css/style-general.css"/>
-        <link type="text/css" media="screen" rel="stylesheet" href="<?= base_url() ?>assets/css/hope-general.css"/>
-        <link type="text/css" media="screen" rel="stylesheet" href="<?= base_url() ?>assets/css/left_menu_style.css"/>
-        <link type="text/css" media="screen" rel="stylesheet" href="<?= base_url() ?>assets/css/smoothness/jquery-ui-1.7.2.custom.css"/>
-        <link type="text/css" media="screen" rel="stylesheet" href="<?= base_url() ?>assets/css/main_decorator.css"/>
+        <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url() ?>assets/css/style-general.css"/>
+        <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url() ?>assets/css/hope-general.css"/>
+        <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url() ?>assets/css/left_menu_style.css"/>
+        <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url() ?>assets/css/smoothness/jquery-ui-1.7.2.custom.css"/>
+        <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url() ?>assets/css/main_decorator.css"/>
 
         <?php foreach($page_decorator->getCssFiles() as $id => $file) { ?>
         <link type="text/css" media="screen" rel="stylesheet" href="<?php echo base_url()."assets/".$file; ?>"/>
         <?php } ?>
 
-        <script type="text/javascript" src="<?= base_url()?>assets/js/jquery/jquery.min.js"></script>      
+        <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery/jquery.min.js"></script>
     </head>
     <body>
         <div>
             <?= $page_content ?>
         </div>
-        <script type="text/javascript" src="<?= base_url()?>assets/js/jquery/jquery-ui-1.7.2.custom.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url()?>assets/js/jquery/jquery-ui-1.7.2.custom.min.js"></script>
         <?php foreach($page_decorator->getScriptFiles() as $id => $file) { ?>
         <script type="text/javascript" src="<?php echo base_url()."assets/".$file; ?>"></script>
         <?php } ?>

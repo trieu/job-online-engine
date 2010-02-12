9f30401f823a6e57f9314181e897b7c08302762a
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-02-12 18:08:16 +0000

    update test for Gdata Google Doc mashup

--- trunk/job-online-website/application/controllers/test/gdata.php
+++ trunk/job-online-website/application/controllers/test/gdata.php
@@ -4,6 +4,7 @@
  * Description of home
  *
  * @property page_decorator $page_decorator
+ * @property gdata_spreadsheet $gdata_spreadsheet
  *
  * @author Trieu Nguyen. Email: tantrieuf31@gmail.com
  */
@@ -16,11 +17,29 @@ class gdata extends Controller {
      * @Decorated
      */
     public function index() {
+        $this->load->library('gdata_spreadsheet');
+        $data = array();
         $this->page_decorator->setPageTitle("Job Management System at DRD");
 
+        $email = "tantrieuf31.database@gmail.com";
+        $pass = "Mycatisfat@31";
+        $spreadsheetId = "tPF0KO8zz8gIB75IZB6fMcQ";
+        $worksheetId = "od6";
+        $this->gdata_spreadsheet->connect($email, $pass, $spreadsheetId, $worksheetId);
+        $this->gdata_spreadsheet->countRowAndCollumn();
+        $data["getRowCount1"] =  $this->gdata_spreadsheet->getRowCount();
+        $data["getColumnCount1"] =  $this->gdata_spreadsheet->getColumnCount();
+
+        $rowArray = array();
+        $rowArray["id"] = 55;
+        $rowArray["name"] = "Trieu 2.55";
+        $data["insertRowIntoWorkSheet"] = $this->gdata_spreadsheet->insertRowIntoWorkSheet($rowArray);
+
+        $this->gdata_spreadsheet->countRowAndCollumn();
+        $data["getRowCount2"] =  $this->gdata_spreadsheet->getRowCount();
+        $data["getColumnCount2"] =  $this->gdata_spreadsheet->getColumnCount();
 
 
-        $data = array();
         $this->load->view("mashup/spreadsheet_view",$data);
     }
 
---
+++ trunk/job-online-website/application/libraries/gdata_spreadsheet.php
@@ -0,0 +1,132 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Zend library include path
+set_include_path(get_include_path() . PATH_SEPARATOR . "$_SERVER[DOCUMENT_ROOT]/job-online-website/application/libraries");
+
+require_once 'Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Gdata');
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+Zend_Loader::loadClass('Zend_Gdata_Spreadsheets');
+Zend_Loader::loadClass('Zend_Gdata_App_AuthException');
+Zend_Loader::loadClass('Zend_Http_Client');
+
+/**
+ * Simple library for Google Spreadsheet API
+ * @author Trieu Nguyen (tantrieuf31@gmail.com)
+ */
+class gdata_spreadsheet {
+
+    private $gdClient;
+    private $currSpreadsheetId = '';
+    private $currWorkSheetId = '';
+    private $rowCount = 0;
+    private $columnCount = 0;
+
+    /**
+     * Constructor
+     *
+     * @param	string $class class name
+     */
+    function __construct($params = array()) {
+
+    }
+
+    /**
+     * connect to Google Spreadsheet
+     *
+     * @param  string $email
+     * @param  string $password
+     * @param  string $spreadsheetId
+     * @param  string $worksheetId
+     * @return void
+     */
+    public function connect($email, $password, $spreadsheetId, $worksheetId) {
+        try {
+            $client = Zend_Gdata_ClientLogin::getHttpClient($email, $password, Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME);
+            $this->gdClient = new Zend_Gdata_Spreadsheets($client);
+            $this->currSpreadsheetId = $spreadsheetId;
+            $this->currWorkSheetId = $worksheetId;
+        } catch (Zend_Gdata_App_AuthException $ae) {
+            exit("Error: ". $ae->getMessage() ."\nCredentials provided were email: [$email] and password [$password].\n");
+        }
+    }
+
+    /**
+     * getRowAndColumnCount
+     *
+     * @return void
+     */
+    public function countRowAndCollumn() {
+        $query = new Zend_Gdata_Spreadsheets_CellQuery();
+        $query->setSpreadsheetKey($this->currSpreadsheetId);
+        $query->setWorksheetId($this->currWorkSheetId);
+        $feed = $this->gdClient->getCellFeed($query);
+
+        if ($feed instanceOf Zend_Gdata_Spreadsheets_CellFeed) {
+            $this->rowCount = $feed->getRowCount();
+            $this->columnCount = $feed->getColumnCount();
+        }
+    }
+
+    public function getCurrSpreadsheetId() {
+        return $this->currSpreadsheetId;
+    }
+
+    public function getCurrWorkSheetId() {
+        return $this->currWorkSheetId;
+    }
+
+    public function getRowCount() {
+        return $this->rowCount;
+    }
+
+    public function getColumnCount() {
+        return $this->columnCount;
+    }
+
+    public function insertRowIntoWorkSheet($rowArray) {
+        $entry = $this->gdClient->insertRow($rowArray, $this->currSpreadsheetId, $this->currWorkSheetId);
+        if ($entry instanceof Zend_Gdata_Spreadsheets_ListEntry) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function updateRowIntoWorkSheet($index, $rowArray ) {
+        $query = new Zend_Gdata_Spreadsheets_ListQuery();
+        $query->setSpreadsheetKey($this->currSpreadsheetId);
+        $query->setWorksheetId($this->currWorkSheetId);
+        $listFeed = $this->gdClient->getListFeed($query);
+        $entry = $this->gdClient->updateRow($listFeed->entries[$index], $rowArray);
+
+        if ($entry instanceof Zend_Gdata_Spreadsheets_ListEntry) {
+            $response = $entry->save();
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+
+    /**
+     * printFeed
+     *
+     * @param  Zend_Gdata_Gbase_Feed $feed
+     * @return void
+     */
+    public function printFeed($feed) {
+        $i = 0;
+        foreach($feed->entries as $entry) {
+            ApplicationHook::logInfo( "<br/>id: ".$entry->id->text."<br/>");
+            if ($entry instanceof Zend_Gdata_Spreadsheets_CellEntry) {
+                ApplicationHook::logInfo( $entry->title->text .' '. $entry->content->text . "<br/>");
+            } else if ($entry instanceof Zend_Gdata_Spreadsheets_ListEntry) {
+                ApplicationHook::logInfo($i .' '. $entry->title->text .' | '. $entry->content->text . "<br/>");
+            } else {
+                ApplicationHook::logInfo( $i .' '. $entry->title->text . "<br/>");
+            }
+            $i++;
+        }
+    }
+}
+
+?>

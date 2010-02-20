deea80d34434140a66b3b2ff65494973bc4b1dfb
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-02-20 04:09:00 +0000

    update search

--- job-online-website/application/views/decorator/admin_page_template.php
+++ job-online-website/application/views/decorator/admin_page_template.php
@@ -32,6 +32,8 @@
                 
                 <div style="font-weight:bold; margin: 10px 0px;">
                     <?php echo anchor('', lang('home_page')); ?>   |
+                    <?php echo anchor('admin/object_controller/list_all/1', lang('job_seeker'), array("title"=>lang('job_seeker_a_title'))); ?>   |
+                    <?php echo anchor('admin/object_controller/list_all/2', lang('employer'), array("title"=>lang('employer_a_title'))); ?>   |
                     <?php echo anchor('admin/admin_panel', lang('admin_panel')); ?>
                 </div>
                 <div class="box accessBox has-access">

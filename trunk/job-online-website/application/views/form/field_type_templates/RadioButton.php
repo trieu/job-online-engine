08260fafdcef3e9eab7d5738c4f2a08fe18518fe
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-07-24 10:22:53 +0000

    fix bugs validation for form

--- trunk/job-online-website/application/views/form/field_type_templates/RadioButton.php
+++ trunk/job-online-website/application/views/form/field_type_templates/RadioButton.php
@@ -5,7 +5,7 @@
             $id = get_random_password();
      ?>
         <div class="radiobutton_wrapper">
-            <input type="radio" id="<?= $id  ?>" name="<?= $field_name ?>" value="<?= $key ?>" />
+            <input type="radio" id="<?= $id  ?>" name="<?= $field_name ?>" value="<?= $key ?>" class="<?= $rules ?>" />
             <label for="<?= $id  ?>" class="vietnamese_english" ><?= $value  ?></label>
         </div>
     <?php } ?>
--- trunk/job-online-website/application/views/form/field_type_templates/TextArea.php
+++ trunk/job-online-website/application/views/form/field_type_templates/TextArea.php
@@ -1,4 +1,4 @@
 <div class="textarea_wrapper">
     <label for="<?= $field_name ?>" class="vietnamese_english" ><?= $field_label ?></label>
-    <textarea id ="<?= $field_name ?>" name="<?= $field_name ?>" rows="4" cols="20"><?= $field_value ?></textarea>
+    <textarea id ="<?= $field_name ?>" name="<?= $field_name ?>" rows="4" cols="20" class="<?= $rules ?>" ><?= $field_value ?></textarea>
 </div>

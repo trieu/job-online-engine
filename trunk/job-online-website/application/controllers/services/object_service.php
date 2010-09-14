01649dac3135bfed64c92f948156e80644b71388
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-09-14 11:11:21 +0000

    update object_service for data export/import

--- trunk/job-online-website/application/controllers/services/object_service.php
+++ trunk/job-online-website/application/controllers/services/object_service.php
@@ -35,57 +35,64 @@ class object_service extends Controller {
     /**
      * 
      */
-    public function import_data($ObjectClassID = -1) {
-        $url1 = 'http://tantrieuf31.summerhost.info/job-database/index.php/services/object_service/export_data/'.$ObjectClassID;
-        $url2 = 'http://drd-vn-database.com/index.php/services/object_service/export_data/'.$ObjectClassID;
-        $url3 = 'http://localhost/job-online-website/tiengviet.php/services/object_service/export_data/'.$ObjectClassID;
+    public function import_data($ObjectClassID = -1, $justView = TRUE) {
+        $url1 = 'http://tantrieuf31.summerhost.info/job-database/index.php/services/object_service/export_data/' . $ObjectClassID;
+        $url2 = 'http://drd-vn-database.com/index.php/services/object_service/export_data/' . $ObjectClassID;
+        $url3 = 'http://localhost/job-online-website/tiengviet.php/services/object_service/export_data/' . $ObjectClassID;
         $out = '';
 
         $rawData = $this->curl->loadHtml($url1);
-        echo "<BR><BR> $rawData <BR><BR>";
-        return;
+        //echo "$rawData <BR><BR>";
+
         $this->load->model("object_manager");
         $rows = json_decode($rawData);
 
         $objects = array();
         foreach ($rows as $row) {
             $objID = $row->ObjectID;
-            if ( $this->object_manager->isObjectExisted($objID)) {
-                if ( ! isset($objects[$objID])) {
-                    $objects[$objID] =  array();
-                    $objects[$objID]['FieldValues'] = array();
-                }
-                $FieldValue = new stdClass();
-                $FieldValue->FieldID = $row->FieldID;
-                $FieldValue->FieldValueID = 0;
-                $FieldValue->FieldValue = $row->FieldValue;
-                $FieldValue->SelectedFieldValue = ($row->SelectedFieldValue == "1") ? true : false;
-                array_push($objects[$objID]['FieldValues'], $FieldValue);
+            if (!isset($objects[$objID])) {
+                $objects[$objID] = array();
+                $objects[$objID]['FieldValues'] = array();
+                $objects[$objID]['IdentityFieldValues'] = array();
+            }
+            $FieldValue = new stdClass();
+            $FieldValue->FieldID = $row->FieldID;
+            $FieldValue->FieldValueID = 0;
+            $FieldValue->FieldValue = $row->FieldValue;
+            $FieldValue->SelectedFieldValue = ($row->SelectedFieldValue == "1") ? true : false;
+            array_push($objects[$objID]['FieldValues'], $FieldValue);
+
+            if ($row->FieldID == "112") {
+                $objects[$objID]['IdentityFieldValues'][$row->FieldID] = $row->FieldValue;
             }
         }
 
         $insertedObjNum = 0;
         //{"FieldID":112,"FieldValueID":0,"FieldValue":"tester","SelectedFieldValue":false}
         foreach ($objects as $id => $impoterdObj) {
-            if ($ObjectClassID > 0) {
+            if ($this->object_manager->isObjectNotExisted($id, $impoterdObj['IdentityFieldValues'])) {
                 $out .= ( json_encode($impoterdObj['FieldValues']) . " -> Import $id <BR><BR>" );
-//                $obj = new Object();
-//                $obj->setObjectClassID($ObjectClassID);
-//                $obj->setObjectID(-1); // add as new
-//                $obj->setFieldValues($impoterdObj['FieldValues']);
-//                $insertedId = $this->object_manager->save($obj);
-//                if ($insertedId > 0) {
-//                    $out .= ("Import $id to new $insertedId <BR>" );
-//                    $insertedObjNum++;
-//                }
+                if ($justView) {
+                    $insertedObjNum++;
+                } else {
+                    $obj = new Object();
+                    $obj->setObjectClassID($ObjectClassID);
+                    $obj->setObjectID(-1); // add as new
+                    $obj->setFieldValues($impoterdObj['FieldValues']);
+                    $insertedId = $this->object_manager->save($obj);
+                    if ($insertedId > 0) {
+                        $out .= ( "Import $id to new $insertedId <BR>" );
+                        $insertedObjNum++;
+                    }
+                }
             }
         }
-        if($insertedObjNum == count($objects)) {
-            $out .= (" ### Success! $insertedObjNum <BR>" );
+        if ($insertedObjNum == count($objects)) {
+            $out .= ( " ### Success! $insertedObjNum <BR>" );
         }
 
         echo $out;
-       // echo json_encode($objects);
+        echo "<BR> Need to import $insertedObjNum / " . (count($objects)) . " objects";
     }
 
     public function simple_get() {

fba9909b9619bc837ed745fc337528a654262df6
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2010-11-16 11:33:53 +0000

    add cloud_storage_manager

---
+++ trunk/job-online-website/application/models/cloud_storage_manager.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'application/libraries/class_mapper.php';
+
+/**
+ *
+ * @property CI_Loader $load
+ * @property CI_DB_active_record $db
+ */
+class cloud_storage_manager extends Model {
+
+    private $table_name = 'cloud_authentications';
+
+    public function __construct() {
+        parent::__construct();
+        $this->class_mapper = new class_mapper();
+    }
+
+    //usually the first one
+    public function getDefaultAccount() {
+        $this->db->select("$this->table_name.*");
+        $this->db->from($this->table_name);
+        $this->db->where(array('provider' => 'google.com'));
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function addLoginAccount($login, $pass, $provider = 'google.com') {
+        $data = array(
+            'login' => $login,
+            'password' => $pass,
+            'provider' => $provider
+        );
+        $this->db->insert($this->table_name, $data);
+    }
+
+}
+
+?>

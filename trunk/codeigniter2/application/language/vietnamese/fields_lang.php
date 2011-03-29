ca2df5dbd4e624759d3316a258c16b7a441551af
tantrieuf31 <tantrieuf31@ab511a90-b563-11de-b1df-31c676d11d27>
2011-03-29 12:11:13 +0000

    move the core FIS to new CodeIgniter 2.0

---
+++ trunk/codeigniter2/application/language/vietnamese/fields_lang.php
@@ -0,0 +1,42 @@
+<?php
+
+//////////////////  Home page  ////////////////////
+$lang['home_page_heading'] = "DRD Databases";
+$lang['admin_page_heading'] = "Quản lý hệ thống";
+
+$lang['home_page'] = "Trờ về trang chủ";
+
+$lang['job_seeker'] = "Người tìm việc";
+$lang['job_seeker_a_title'] = "Trang quản lý các chức năng dành cho người tìm việc";
+$lang['list_job_seeker'] = "Liệt kê tất cả Người tìm việc";
+$lang['list_job_seeker_a_title'] = "Liệt kê tất cả Người tìm việc";
+$lang['employer'] = "Nhà tuyển dụng";
+$lang['employer_a_title'] = "Danh sách các nhà tuyển dụng";
+$lang['list_employer'] = "Liệt kê tất cả Nhà tuyển dụng";
+$lang['list_employer_a_title'] = "Liệt kê tất cả Nhà tuyển dụng";
+
+$lang['job'] = "Danh sách công việc";
+$lang['job_a_title'] = "Danh sách công việc";
+$lang['search'] = "Tìm kiếm";
+$lang['search_title'] = "Tìm kiếm - Thống kê các thông tin từ database";
+$lang['databases'] = "Danh sách Database";
+$lang['databases_title'] = "Danh sách các Databases có sẵn trong hệ thống";
+$lang['database_tools'] = "Công cụ Database";
+$lang['database_tools_title'] = "Các công cụ trợ giúp quản lý - Tìm kiếm - Thống kê cho database";
+$lang['options'] = "Các lựa chọn";
+
+$lang['news_events'] = "Tin tức - Sự kiện";
+$lang['contact'] = "Liên hệ";
+$lang['admin_panel'] = "Database Admin Panel";
+
+$lang['register_new_employer'] = "Đăng ký mới nhà tuyển dụng";
+$lang['register_new_jobseeker'] = "Đăng ký mới người tìm việc";
+
+//search view
+$lang['no_result_text'] = "Không có kết quản nào được tìm thấy!";
+$lang['result_number_label'] = "Số lượng kết quả tìm thấy: ";
+
+//form object
+$lang['form_edit_details'] = "Chỉnh sửa";
+$lang['form_view'] = "Giao diện form";
+?>
---
+++ trunk/codeigniter2/application/libraries/page_decorator.php
@@ -0,0 +1,66 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * page_decorator
+ */
+class page_decorator {
+
+    protected $CI;
+    protected $pageTitle = "";
+    protected $pageMetaTags= array();
+    protected $scriptFiles = array();
+    protected $cssFiles = array();
+
+    /**
+     * __construct
+     *
+     * @return void
+     * @author Trieu Nguyen
+     **/
+    public function __construct() {
+        $this->CI =& get_instance();
+        $pageMetaTags = array();
+    }
+
+    public function setPageMetaTag($name,$content) {
+        $this->pageMetaTags[$name] = $content;
+    }
+
+    public function getPageTitle() {
+        $this->pageTitle = lang('home_page_heading')." - ".$this->pageTitle;
+        return $this->pageTitle;
+    }
+
+    public function getPageMetaTags() {
+        return $this->pageMetaTags;
+    }
+
+    public function setPageTitle($pageTitle) {
+        $pageTitle = show_the_excerpt(trim($pageTitle), 12);
+        $this->pageTitle = ($pageTitle);
+    }
+
+    public function getScriptFiles() {
+        return $this->scriptFiles;
+    }
+
+    public function addScriptFile($relative_path) {
+        array_push($this->scriptFiles, $relative_path);
+    }
+
+    public function addScriptFiles( $relative_paths = array() ) {
+        $this->scriptFiles = $relative_paths;
+    }
+
+    public function getCssFiles() {
+        return $this->cssFiles;
+    }
+
+    public function addCssFile($relative_path) {
+        array_push($this->cssFiles, $relative_path);
+    }
+
+    public function addCssFiles( $relative_paths = array() ) {
+        $this->cssFiles = $relative_paths;
+    }
+}

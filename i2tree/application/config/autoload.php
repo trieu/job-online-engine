a989b30d644c515840bb16966e7c548e4a59db8c
tantrieuf31@gmail.com <tantrieuf31@gmail.com@ab511a90-b563-11de-b1df-31c676d11d27>
2012-04-02 10:00:11 +0000

    i2tree alpha 1

--- i2tree/application/config/autoload.php
+++ i2tree/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database', 'session', 'redux_auth', 'form_validation','email','page_decorator');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'form', 'html','language','page_decorator');
 
 
 /*
@@ -80,7 +80,7 @@ $autoload['helper'] = array();
 |
 */
 
-$autoload['config'] = array('zend_ci_config');
+$autoload['config'] = array('redux_auth', 'decorator','zend_ci_config');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('redux_auth_model');
 
 
 /* End of file autoload.php */
--- i2tree/application/config/hooks.php
+++ i2tree/application/config/hooks.php
@@ -9,7 +9,21 @@
 |	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['post_controller_constructor'] = array(
+                                'class'    => 'ApplicationHook',
+                                'function' => 'checkRole',
+                                'filename' => 'ApplicationHook.php',
+                                'filepath' => 'hooks',
+                                'params'   => array()
+                                );
 
+$hook['display_override'] = array(
+                                'class'    => 'ApplicationHook',
+                                'function' => 'decoratePage',
+                                'filename' => 'ApplicationHook.php',
+                                'filepath' => 'hooks',
+                                'params'   => array()
+                                );
 
 
 /* End of file hooks.php */

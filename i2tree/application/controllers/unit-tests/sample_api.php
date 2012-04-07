5b61bf8b3e4094c51bee907e2a3fd9ef38ca4e8b
tantrieuf31@gmail.com <tantrieuf31@gmail.com@ab511a90-b563-11de-b1df-31c676d11d27>
2012-04-07 07:39:26 +0000

    i2tree Alpha 3 refactoring code unit tests

--- i2tree/application/controllers/unit-tests/sample_api.php
+++ i2tree/application/controllers/unit-tests/sample_api.php
@@ -5,30 +5,29 @@ if (!defined('BASEPATH'))
 
 class sample_api extends CI_Controller {
 
-    /**
-     * @Decorated
-     */
-    public function index() {
-        //$this->load->view('welcome_message');      
-        $this->page_decorator->setPageTitle("i2tree: sample api test");
-
-        $output = 'sample api test';
-        $this->output->set_output($output);
+    function __construct() {
+        parent::__construct();
     }
 
     private $sample_users = array(
         3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!', array('hobbies' => array('fartings', 'bikes'))),
         4 => array('id' => 4, 'name' => 'Nguyễn Tấn Triều', 'email' => 'tantrieuf31@gmail.com', 'fact' => 'a software engineer')
     );
-    
-    
+
+    /**
+     * @Api
+     */
+    public function index() {
+        $output = json_encode($this->sample_users);
+        $this->output->set_output($output);
+    }
+
     //1: GET http://localhost/i2tree/index.php/oauth/index?client_id=hello-i2tree&redirect_uri=http://localhost/i2tree/index.php/unit-tests/&response_type=code&client_secret=e10adc3949ba59abbe56e057f20f883e&scope=user.details&state=04042012
     //=> localhost/i2tree/index.php/unit-tests/?code=06776d83dfb1d36d8b660046c6cb6a28&state=
     //2: POST http://localhost/i2tree/index.php/oauth/access_token
     // params: client_id=hello-i2tree&client_secret=e10adc3949ba59abbe56e057f20f883e&redirect_uri=http%3A%2F%2Flocalhost%2Fi2tree%2Findex.php%2Funit-tests%2F&code=11aef0dd38b251150109b47ac91be867&grant_type=authorization_code
     //=> {"access_token":"3916f65b0af4687b30da58048815fab67416a874","error":0,"error_message":""}
     //3: GET: http://localhost/i2tree/index.php/unit-tests/sample_api/classified_persons?access_token=3916f65b0af4687b30da58048815fab67416a874
-    
 
     /**
      * @Api
@@ -40,14 +39,6 @@ class sample_api extends CI_Controller {
             $this->output->set_output(json_encode(array('error_message' => 'access token does not have correct permission to user this API method')));
             return;
         }
-        $output = json_encode($this->sample_users);
-        $this->output->set_output($output);
-    }
-
-    /**
-     * @Api
-     */
-    public function persons() {
         $output = json_encode($this->sample_users);
         $this->output->set_output($output);
     }

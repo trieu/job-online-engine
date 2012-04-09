6015b9e3d1d38e0e9359e90cc300fc4857b9de15
tantrieuf31@gmail.com <tantrieuf31@gmail.com@ab511a90-b563-11de-b1df-31c676d11d27>
2012-04-09 16:58:16 +0000

    i2tree Alpha 5 + crawler_api

---
+++ trunk/i2tree/application/views/unit-tests/crawler_data_view.php
@@ -0,0 +1,29 @@
+<a id="data_url" href="#"></a>
+<div id="data_view" style="display: none;"></div>
+
+<script type="text/javascript" >
+    var setDataCallback = function(data) {
+        jQuery('#data_url').html(data.url).attr('href',data.url);       
+        jQuery('#data_view').html(data.content);       
+        console.log("setDataCallback: "  );
+    };
+    
+    jQuery.cachedScript = function(url, options) {
+        // allow user to set any option except for dataType, cache, and url
+        options = $.extend(options || {}, {
+            dataType: "script",
+            cache: true,
+            url: url,
+            error: function() {               
+                jQuery('#data_view').html('Resource is not found!').show();  
+            }
+        });
+        // Use $.ajax() since it is more flexible than $.getScript
+        // Return the jqXHR object so we can chain callbacks
+        return jQuery.ajax(options);
+    };
+
+    jQuery.cachedScript('<?php echo $data_url ?>').done(function(script, textStatus) {
+        jQuery('#data_view').slideDown();
+    });
+</script>
